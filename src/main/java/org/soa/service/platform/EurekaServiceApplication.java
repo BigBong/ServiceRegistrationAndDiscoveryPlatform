4403ecc0c2c40c401e9782c311027ef957aa9ea7
xuwei19 <xuwei19@baidu.com>
2017-08-04 11:30:52 +0800

    improve

--- src/main/java/org/soa/service/platform/EurekaServiceApplication.java
+++ src/main/java/org/soa/service/platform/EurekaServiceApplication.java
@@ -1,5 +1,9 @@
 package org.soa.service.platform;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Created by frodo on 2017/8/3.
  */
